af6f71306c644b311d29c6c4b3d90280574af937
Bruna Hornink <bhornink@gmail.com>
2024-04-02 20:34:11 -0300

    create full data base

--- data_proc.R
+++ data_proc.R
@@ -1,58 +1,74 @@
-
+#Clean workspace
 rm(list = ls())
 
-#1.Diretório####
+#1.Set Directory####
 
 wd <- "C:/Documentos/PhD/Projeto/Abordagem 1.b/mensuração densidade"
 setwd(wd)
 dir()
 
-#2.Pacotes####
-#install.packages("xRing")
+#2.Packages required####
+#install.packages("")
 library(tidyverse)
 library(lme4)
 
-#3.Leitura dos dados####
+#3.Reading database####
 
-#leitura das pastas que contem _output para conseguir acessar os dados por espécie
-name_files <- list.files(wd, pattern = "\\_output")
-length(name_files)
+#reading all the file with "_output" in the name inside of the Output file
+dir()
+dir_files <- file.path(wd,"Output")
+list_files <- list.files(dir_files)
+length(list_files)
 
-#criei uma base vazia
+#create an empty data frame
 base_total <- data.frame()
 
-# length(name_files)
-# name_data <- list.files(name_files[1], pattern = "\\.txt$")
-# length(name_data) #1:44
-# file_path <- file.path(name_files[1], name_data[15])
+#test the code
+#name_data <- list.files(file.path(dir_files,list_files[1]), pattern = "\\.txt$")
+# file_path <- file.path(dir_files,list_files[1],name_data[1])
 # data_file <-  read.table(file_path, sep = ";", header = TRUE)
-# head(data_file)
+# data_file$species <- paste(sub("\\_output$", "", file))
+# data_file$position <- substr(data_file$Cod, nchar(data_file$Cod) - 1, nchar(data_file$Cod)-1) 
+# data_file$ray <- substr(data_file$Cod, nchar(data_file$Cod), nchar(data_file$Cod))
+# data_file$cod_arv <- substr(data_file$Cod,1,nchar(data_file$Cod) - 2)
 
-for (file in 1:length(name_files)) {
-  
-  name_data <- list.files(name_files[file], pattern = "\\.txt$") #seleciono uma das pastas e leio somente os arquivos txt que tem dentro
-  
-  for (data in 1:length(name_data)) { #vamos selecionar cada arquivo dentro da pasta para montar a base total
+for (file in 1:length(list_files)) {
     
-    file_path <- file.path(name_files[file], name_data[data])
-    data_file <-  read.table(file_path, sep = ";", header = TRUE) 
+    #select a file and list all the file .txt inside of it  
+    name_data <-  list.files(file.path(dir_files,list_files[file]), pattern = "\\.txt$") 
+  
+  #select the each database inside of the file
+  for (data in 1:length(name_data)) { 
+    
+    # database path
+    data_path <- file.path(dir_files,list_files[file],name_data[data])
+    #read the database 
+    data_file <-  read.table(data_path, sep = ";", header = TRUE) 
    
-    data_file$species <- paste(sub("\\_output$", "", file)) #nome da espécie vai vir do nome da pasta
-    data_file$position <- substr(data_file$Cod, nchar(data_file$Cod) - 1, nchar(data_file$Cod)-1) #a posição vai vir do código da amostr obtida no script medição_raiosX
-    data_file$ray <- substr(data_file$Cod, nchar(data_file$Cod), nchar(data_file$Cod))#o raio vai vir do código da amostr obtida no script medição_raiosX
-    data_file$cod_arv <-  substr(data_file$Cod,1,3)#o código da árvore vai vir do código da amostr obtida no script medição_raiosX
+    #add specie name using the file name
+    data_file$species <- paste(sub("\\_output$", "", list_files[file])) 
+    
+    #add height position (base, middle, top)
+    data_file$position <- substr(data_file$Cod, nchar(data_file$Cod) - 1, nchar(data_file$Cod)-1) 
+    
+    #add the number of the ray 
+    data_file$ray <- substr(data_file$Cod, nchar(data_file$Cod), nchar(data_file$Cod))
+    
+    substr(data_file$Cod,1,nchar(data_file$Cod) - 2)#o código da árvore vai vir do código da amostr obtida no script medição_raiosX
     
     base_total <- rbind(base_total, data_file) #criei a base final juntando todas as bases na base vazia
 
   }
-  
 }
 
 str(base_total)
-unique(base_total$Cod)
-length(unique(base_total$Cod)) #confirmando se todos os arquivos foram lidos
+unique(base_total$species)
+length(unique(base_total$Cod)) 
 
-base_total$Cod[which(is.na(base_total$position))] #verificando se algum ficou com NA 
+#check if any position has na
+base_total$Cod[which(is.na(base_total$position))]
+
+write.table(base_total,file.path(wd,"base_total.txt"), sep = ";")
 
 
 #4. Verificando qualidade de dados ####
